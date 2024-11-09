0929da0e0788d7d48e784ebc46e9663ac092eebb
Muzto <am240755@gmail.com>
2024-11-10 04:13:34 +0700

    fix bug host value

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,12 +1,9 @@
 import pluginJs from '@eslint/js';
-import daStyle from 'eslint-config-dicodingacademy';
 import globals from 'globals';
 
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-  daStyle,
-
   { files: ['**/*.js'], languageOptions: { sourceType: 'commonjs' } },
   { languageOptions: { globals: globals.browser } },
   pluginJs.configs.recommended,
